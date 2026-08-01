89f244385f32c937fb255899021b16589f28f8f2
HelixCraft <HelixCraft@users.noreply.github.com>
2026-08-01 10:05:33 +0200

    Use simpleicons for social icons

--- script.js
+++ script.js
@@ -495,19 +495,19 @@ const pages = {
           </div>
           <div class="hero-links">
             <a class="btn btn-ghost" href="https://github.com/HelixCraft" target="_blank">
-              <svg width="13" height="13" viewBox="0 0 16 16" fill="currentColor"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"/></svg>
+              <img class="simple-icon" src="https://cdn.simpleicons.org/github" alt="GitHub" width="13" height="13">
               GitHub
             </a>
             <a class="btn btn-ghost" href="https://modrinth.com/user/HelixCraft" target="_blank">
-              <img class="modrinth-icon" src="https://upload.wikimedia.org/wikipedia/commons/3/38/Modrinth_Logo.webp" alt="Modrinth" width="13" height="13">
+              <img class="simple-icon" src="https://cdn.simpleicons.org/modrinth" alt="Modrinth" width="13" height="13">
               Modrinth
             </a>
             <a class="btn btn-ghost" href="https://discordapp.com/users/1502297205845790781" target="_blank">
-              <svg width="13" height="13" viewBox="0 0 24 24" fill="currentColor"><path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.25.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057.1 18.08.114 18.104.133 18.12a19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028c.462-.63.874-1.295 1.226-1.994a.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03z"/></svg>
+              <img class="simple-icon" src="https://cdn.simpleicons.org/discord" alt="Discord" width="13" height="13">
               Discord
             </a>
             <a class="btn btn-ghost" href="mailto:HelixCraft@tuta.io">
-              <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="4" width="20" height="16" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>
+              <img class="simple-icon" src="https://cdn.simpleicons.org/gmail" alt="Email" width="13" height="13">
               Email
             </a>
           </div>
@@ -581,19 +581,19 @@ const pages = {
       </div>
       <div class="section-label">Links</div>
       <a class="social-link" href="https://modrinth.com/user/HelixCraft" target="_blank">
-        <img class="modrinth-icon" src="https://upload.wikimedia.org/wikipedia/commons/3/38/Modrinth_Logo.webp" alt="Modrinth" width="17" height="17">
+        <img class="simple-icon" src="https://cdn.simpleicons.org/modrinth" alt="Modrinth" width="17" height="17">
         <div class="social-text"><strong>Modrinth</strong>modrinth.com/user/HelixCraft</div>
       </a>
       <a class="social-link" href="https://github.com/HelixCraft" target="_blank">
-        <svg width="17" height="17" viewBox="0 0 16 16" fill="var(--text2)"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"/></svg>
+        <img class="simple-icon" src="https://cdn.simpleicons.org/github" alt="GitHub" width="17" height="17">
         <div class="social-text"><strong>GitHub</strong>github.com/HelixCraft</div>
       </a>
       <a class="social-link" href="https://discordapp.com/users/1502297205845790781" target="_blank">
-        <svg width="17" height="17" viewBox="0 0 24 24" fill="var(--text2)"><path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.25.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057.1 18.08.114 18.104.133 18.12a19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028c.462-.63.874-1.295 1.226-1.994a.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03z"/></svg>
+        <img class="simple-icon" src="https://cdn.simpleicons.org/discord" alt="Discord" width="17" height="17">
         <div class="social-text"><strong>Discord</strong>1502297205845790781</div>
       </a>
       <a class="social-link" href="mailto:HelixCraft@tuta.io">
-        <svg width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="var(--text2)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="4" width="20" height="16" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>
+        <img class="simple-icon" src="https://cdn.simpleicons.org/gmail" alt="Email" width="17" height="17">
         <div class="social-text"><strong>Email</strong>HelixCraft@tuta.io</div>
       </a>
     </div>
